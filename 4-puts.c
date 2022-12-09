b8a2cbc216251aea838470847de98ef73daa9b70
6ftJagaban <asgorleku@icloud.com>
2022-12-08 21:19:23 -0800

    Project

---
+++ 4-puts.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point
+ * print "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Successs)
+ */
+#include <stdio.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+		return (0);
+}
