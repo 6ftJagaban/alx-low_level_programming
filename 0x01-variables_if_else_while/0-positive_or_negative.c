b5ca18d82390b2ddb8790820b4af971caa7110cf
6ftJagaban <asgorleku@icloud.com>
2022-12-09 19:40:58 -0800

    C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 /**
  * main - ALXD coding project
- * DESCRIPTION - a code that prints the value of a random number
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -13,17 +12,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
 	    printf("%d is poitive\n", n);
-	}
 	else if (n < 0)
-	{
 	    printf("%d is negative\n", n);
-	}
 	else 
-	{
 	    printf("%d is zero\n", n);
-	}
-	
 	return (0);
 }
