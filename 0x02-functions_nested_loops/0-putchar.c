d75e376cb680446b39459989a024c9f7590b3d2c
6ftJagaban <asgorleku@icloud.com>
2022-12-13 18:06:50 -0800

    Coding

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,16 @@
  * Description - Print "_putchar"
  * Return: Always 0
  */
-char x;
+int main(void)
 {
-	x = "_putchar";
-		_putchar(x"\n");
-		return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
