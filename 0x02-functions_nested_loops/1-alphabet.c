98ea2b21d3d9694f71e5762fd444dfaa5d892967
6ftJagaban <asgorleku@icloud.com>
2022-12-13 18:27:05 -0800

    Coding

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - ALX Coding Project
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char n;
+	
+	n = 'a';
+
+	while (n <= 'z')
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n')
+		return (0);
+}
