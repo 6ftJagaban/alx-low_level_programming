cfbcca4501f1d43e4f35bd713cfad721a88060d9
6ftJagaban <asgorleku@icloud.com>
2022-12-14 00:22:45 -0800

    Coding

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 	int k = 97, g = 122, x = 1;
 
-	while (x < 111)
+	while (x < 11)
 	{
 		while (k <= g)
 		{
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - does something
+ * @a: integer
+ * Return: Always 0
+ */
+int _islower(int a)
+{
+	if (a > 98 && a < 133)
+		return (1);
+	else
+		return (0);
+}
