b9f4522cae6257ee9d9a3175ff32d10e62c35e9e
6ftJagaban <asgorleku@icloud.com>
2023-01-03 15:43:08 -0800

    Coding

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _ptchar(char c);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int _putchar(char c);
 char *_memset(char *s, char*b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *strchr(char *s, char c);
