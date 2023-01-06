dd2f399afe1eda0d7f6ee07815c129c3cb07eec0
6ftJagaban <asgorleku@icloud.com>
2023-01-05 19:52:03 -0800

    Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -17,7 +17,7 @@ int evaluate_num(int num, int iterator)
 	}
 	if (num % iterator != 0)
 	{
-		return (evaluate_num(num. iterator + 1));
+		return (evaluate_num(num, iterator + 1));
 	}
 	return (0);
 }
@@ -28,9 +28,9 @@ int evaluate_num(int num, int iterator)
  */
 int is_prime_number(int num)
 {
-	int iterator
-
-		iterator = 2;
+	int iterator;
+	
+	iterator = 2;
 	if (num < 2)
 	{
 		return (0);
