f843cbbd7e307201bfb8b44b2e06493e30b86387
6ftJagaban <asgorleku@icloud.com>
2023-01-09 17:45:16 -0800

    Arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Prints name of program
+ * @argc: counts argument
+ * @argv:  argument vector
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is to test my knowledge of argument counts and argument vectors of an executable program
